ad821af0f7701e4c7fa32d615f1da66fdcbd0530
kirin111697 <coco41134@hotmail.com>
2016-03-01 21:30:04 +0800

    final code

--- questionb.cpp
+++ questionb.cpp
@@ -2,40 +2,17 @@
 using namespace std;
 
 int main(){
-	int a,b,max;
-	int i,j,c;
-	long long temp;
-	c=1;
-	max=0;
-	while(cin >> a >> b){
-		if (a>b){//make i<j
-			i=b;
-			j=a;
-		}
-		else if (b>a){
-			i=a;
-			j=b;
-		}
+	int a;
+	cin >> a;
+	while(a != 1){
+		cout << a
+			 << " ";
+		if (a%2 != 0)
+			a=3*a+1;
 		else
-			i=j=a;
-		for(;i<=j;i++){
-			temp=i;
-			while(temp!=1){
-				if(temp%2!=0){
-					temp=temp*3+1;
-					c++;
-				}
-				else{
-					temp=temp/2;
-					c++;
-				}	
-			}
-			if(c>max)
-				max=c;
-			c=1;
-		}
-		cout << a << " " << b << " " << max << endl;
-		max=0;
-	}	
+			a=a/2;
+	}
+	cout << a
+		 << endl;
 	return 0;
 }
